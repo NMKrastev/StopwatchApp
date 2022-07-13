577074ea8f739685e21a1ff475d29c78ec5c73f8
Nikola <nikola.malinov.krastev@gmail.com>
2022-07-13 09:50:19 +0300

    Added methods and variables

--- src/Stopwatch.java
+++ src/Stopwatch.java
@@ -1,12 +1,40 @@
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Stopwatch implements ActionListener {
 
+    JFrame frame = new JFrame();
+    JButton startButton = new JButton("Start");
+    JButton resetButton = new JButton("Reset");
+    JLabel timeLabel = new JLabel();
+    int elapsedTime = 0;
+    int seconds = 0;
+    int minutes = 0;
+    int hours = 0;
+    boolean started = false;
+    String seconds_string = String.format("%02d",seconds);
+    String minutes_string = String.format("%02d", minutes);
+    String hours_string = String.format("%02d", hours);
 
+    Stopwatch() {
+
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+
+    }
+
+    void start() {
+
+    }
+
+    void stop() {
+
+    }
+
+    void reset() {
 
     }
 }
